4458af2155d296b951619eb99dfdb139ff5e2f85
PeehWolf <pedro.tso@hotmail.com>
2023-08-31 19:11:37 -0300

    'verification'

--- CO2Cal-Prg/Geodata.py
+++ CO2Cal-Prg/Geodata.py
@@ -6,6 +6,6 @@ def carregar_geodados():
     GDMunicipio = gpd.read_file('Geodata/MS_Municipios_2022.shp')
     GDMesoregiao = gpd.read_file('Geodata/MS_Mesorregioes_2022.shp')
     GDMicrorregiao = gpd.read_file('Geodata/MS_Microrregioes_2022.shp')
-    return GDMunicipio, GDMesoregiao, GDMicrorregiao
-
+    GDAnalise = gpd.read_file('Geodata/Area_de_Analise/FAZ_STO_ANTONIO_PABLO.geojson')
+    return GDMunicipio, GDMesoregiao, GDMicrorregiao, GDAnalise
 
